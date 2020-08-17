c72dc4830a9157eb142495d92539fb7b4ff232ac
clin88 <chen.lin@debtsy.co>
2020-08-17 09:15:49 -0700

    Redacted, unrelated-to-this-repo commit message

--- aws.sh
+++ aws.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
 serial_number=`aws iam list-mfa-devices | jq '.MFADevices[0].SerialNumber' -r`
 
 echo "Enter AWS MFA code: "
